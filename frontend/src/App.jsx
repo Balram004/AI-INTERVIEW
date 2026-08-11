15233559a135788b0299ad4ca743963b70c0ed82
Balram Tomar <142824257+Balram004@users.noreply.github.com>
2026-08-11 12:17:44 +0530

    Updated project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import Home from "./pages/Home";
 import Interview from "./pages/Interview";
 import Report from "./pages/Report";
 import Progress from "./pages/Progress";
-import ResumeTips from "./pages/ResumeTips";
-import CodingRound from "./pages/Codinground";
+import Resumetips from "./pages/ResumeTips";
+import Codinground from "./pages/Codinground";
 
 function PrivateRoute({ children }) {
   const token = localStorage.getItem("token");
@@ -21,8 +21,8 @@ export default function App() {
       <Route path="/interview" element={<PrivateRoute><Interview /></PrivateRoute>} />
       <Route path="/report" element={<PrivateRoute><Report /></PrivateRoute>} />
       <Route path="/progress" element={<PrivateRoute><Progress /></PrivateRoute>} />
-      <Route path="/coding" element={<PrivateRoute><CodingRound /></PrivateRoute>} />
-      <Route path="/resume-tips" element={<PrivateRoute><ResumeTips /></PrivateRoute>} />
+      <Route path="/coding" element={<PrivateRoute><Codinground /></PrivateRoute>} />
+      <Route path="/resume-tips" element={<PrivateRoute><Resumetips /></PrivateRoute>} />
       <Route path="*" element={<Navigate to="/auth" replace />} />
     </Routes>
   );
